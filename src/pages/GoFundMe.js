4313063627fc3163cbef357260a3044cf107789b
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-01-16 17:37:34 -0600

    minor changes

--- src/pages/GoFundMe.js
+++ src/pages/GoFundMe.js
@@ -4,8 +4,14 @@ function GoFundMe(){
     return(
     <div className="container">
       <div>
-        <div className="flex funFact">Fun Fact</div>
-        <div className="quickFunFact">A weird fact about me is that I have an obsession with a Strange Japaneses van. So much so that I even have a GoFundMe set up.</div>
+        <div className="flex funFact">My Dumb Dream</div>
+        <div className="arrow flex" >
+          <span>&#8675;</span>
+        </div>
+        <div className="flex" id="gofundmelink">
+          <a className="link" href="https://gofund.me/bb246245">Click here</a>
+        </div>
+        <div className="quickFunFact"> I have an obsession with this car called</div>
       </div>
      
       <div className="flex">
