aaf135ac4e352d10c83b326be87d08f80ba1be50
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-03-27 20:19:52 -0500

    different backgrounds

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,27 @@
+import React from 'react';
+import backgroundImage from './images/Cube.jpg';
+import style from "./Projects.module.css";
+
+
+
 function Projects(){
     return(
     <div>
-      Projects
+        <div>
+            <style>
+                {`
+                    body {
+                        background-image: url(${backgroundImage});
+                        background-position: center;
+                        background-repeat: no-repeat;
+                        background-size: cover;
+                    }
+                `}
+            </style>
+        </div>
+        <div>
+            best project!
+        </div>
     </div>
     );
 }
