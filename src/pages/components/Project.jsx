3492ebbe5978a9c51b8451ee8ec1487adefc4da1
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-04-17 19:24:14 -0500

    imporved box shadow

--- src/pages/components/Project.jsx
+++ src/pages/components/Project.jsx
@@ -12,12 +12,12 @@ const Project = ({projects}) => {
                     </div>
                     <div className="x3"></div>
                 </div>
-                <div className="flex " id="box">
+                <div className="flex ">
                     <div className="x2">
                         <img src={projects.image} alt={projects.name} />
                     </div>
                     <div className="ReadMe x3 flex center middle">
-                        <div>
+                        <div >
                             
                                 {
                                 //console.log(projects.descriptions)
