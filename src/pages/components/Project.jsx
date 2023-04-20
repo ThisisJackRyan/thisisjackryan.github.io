064a11804fe378844eb1d4db7b1716cd83931cbc
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-04-19 20:33:13 -0500

    imporved image placement

--- src/pages/components/Project.jsx
+++ src/pages/components/Project.jsx
@@ -13,7 +13,7 @@ const Project = ({projects}) => {
                     <div className="x3"></div>
                 </div>
                 <div className="flex ">
-                    <div className="x2">
+                    <div className="x2 projectImage">
                         <img src={projects.image} alt={projects.name} />
                     </div>
                     <div className="ReadMe x3 flex center middle">
@@ -30,14 +30,7 @@ const Project = ({projects}) => {
                                     </>
 
                                 ))}
-
                                 
-                            {/*<span>At FlipSwitch VR we use a software called Haze, which host our games and sends the data out to the other headsets. The goal of this program was to move the Players and their respective machineGuids to the other arena.</span>
-                            <br />
-                            <br />
-                            <span>The part I enjoy about this simple but functional code is that it is my first "Real Life" example of how you can optimize your life with code. While this only saves the host 30 seconds, this is able to run over and over for years to come.</span>
-                            <br />
-                            <br />*/}
                             <div className="LinkToGit">
                                 <a href={projects.gitHub} target="_blank" rel="noreferrer">
                                     <span>To learn more click the link to my github</span>
