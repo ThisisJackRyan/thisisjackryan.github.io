019ea74749683db9644cf966801d7c64b272f9c6
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-05-05 18:55:46 -0500

    simplified GoFundMe.jsx

--- src/pages/GoFundMe.jsx
+++ src/pages/GoFundMe.jsx
@@ -3,24 +3,9 @@
 function GoFundMe(){
     return(
     <div className="container">
-      <div>
-        <div className="flex funFact">My Dumb Dream</div>
-        <div className="arrow flex" >
-          <span>&#8675;</span>
-        </div>
         <div className="flex" id="gofundmelink">
-          <a className="link" href="https://gofund.me/bb246245">Click here</a>
+          <a href="https://gofund.me/bb246245" target="_blank" rel="noreferrer">My Dumb Dream</a>
         </div>
-        <div className="quickFunFact"> I have an obsession with this car called</div>
-      </div>
-     
-      <div className="flex">
-        <div>
-          <div>intro</div>
-          <div>words...</div>
-        </div>
-        <div>img</div>
-      </div>
     </div> 
     );
 }
