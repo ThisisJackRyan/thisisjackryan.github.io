897f43959993f3fc93d2d6f312b34b98509b656d
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-05-09 14:47:22 -0500

    changed Header logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,13 +2,17 @@ import logo from './pages/images/logo.svg';
 import "./index.css";
 import {Link} from "react-router-dom"
 
+//<img src={logo} className="" alt="" />
 
 export default function Header(){
     return (
         <div className="header">
             <div className="flex">
                 <div className="x2 headerPic">
-                    <img src={logo} className="" alt="" />
+                    <span className='myHeaderLogo'>
+                        <span id='Left'>J</span>
+                        <span id='Right'>R</span>
+                    </span>
                 </div>
                 <div className="x1 flex headerTitle"><Link className="link" to="/">ThisisJackRyan.com</Link> </div>
                 <div className="flex nav x2">
