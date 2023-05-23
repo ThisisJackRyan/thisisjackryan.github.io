2a3b9754cb6c8ca294118ea2ab009d05a34608df
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-05-22 22:45:51 -0500

    resized home for <1200px<home.jsx<850px

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ export default function Home(){
     return(
     <div className="container">
       <div className="flex article">
-        <div className="x2 ">{/* this will be my name followed by a brief description of me using the "aspiring software developer" */}
-          <div className="nameTitle flex"><span> Jack Ryan</span></div>{/* I think big blocky-text or squared-text would look great  so far -- Orbitron,  Russo One, Teko, Bebas Neue look great for Jack Ryan on google fonts */}
+        <div className="x2 info">
+          <div className="nameTitle flex"><span> Jack Ryan</span></div>
           <div className="quickDescription">
           Hello, my name is Jack Ryan, and I am an aspiring Software Developer.
           When I'm not busy coding, you can find me outdoors, camping, and rock-climbing. 
@@ -20,7 +20,7 @@ export default function Home(){
           </div>
         </div>
         <div className="x2 homePicture">
-          <img src={PictureOfJack} alt="" />{/* this could be a senior pick of me leaning up against a right wall*/}
+          <img src={PictureOfJack} alt="" />
         </div>
       </div>
     </div>
