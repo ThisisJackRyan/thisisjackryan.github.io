49db11de69d1be97c2e04e0f4994688f042f9b80
Jack Ryan <thisisjackryan1@gmail.com>
2025-06-15 18:13:08 -0500

    try again

--- src/app/steps/ProjectPanel.js
+++ src/app/steps/ProjectPanel.js
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 function ProjectPanel() {
     return (
-        <div id='project' className='h-dvh snap-start'>
-            <div className='flex justify-center border-t max-w-full md:mx-16'>
-                <div className='flex-1/2 my-12 p-4 md:pr-8 md:ml-12 lg:border-r'>
+        <div id='project' className='h-dvh snap-start flex flex-col justify-around border-t md:mx-16'>
+            <div className='flex justify-center max-w-full '>
+                <div className='flex-1/2 mt-12  p-4 md:pr-8 md:ml-12 lg:border-r'>
                     <h2 className='pb-4 text-xl text-center'>Projects</h2>
-                    <p className='text-xl'>I&#39;m always working on something — whether it&#39;s a side project, a school assignment, or something for work. It&#39;s rare that a day goes by where I&#39;m not coding. </p>
+                    <p className='text-sm md:text-xl'>I&#39;m always working on something — whether it&#39;s a side project, a school assignment, or something for work. It&#39;s rare that a day goes by where I&#39;m not coding. </p>
                     <br />
-                    <p className='text-xl'>While I&#39;m still exploring and figuring out my niche in the world of software, web development has been my main focus so far, thanks to both work and personal projects. That said, I do enjoy diving into other areas of software whenever I get the chance.</p>
+                    <p className='text-sm md:text-xl'>While I&#39;m still exploring and figuring out my niche in the world of software, web development has been my main focus so far, thanks to both work and personal projects. That said, I do enjoy diving into other areas of software whenever I get the chance.</p>
                     <div className='flex justify-center items-center m-8 relative group'>
                         {/* TODO: change this href to /projects */}
                         <Link href="#project" className='bg-stone-900 border rounded-4xl px-8 py-4 text-white hover:bg-stone-700'>
@@ -26,7 +26,7 @@ function ProjectPanel() {
                     <Image src='/panels/projects.png' width={500} height={300} className='rounded-lg' alt="coding representation"/>
                 </div>
             </div>
-            <div className='m-8 justify-around flex'>
+            <div className='md:m-8 justify-around flex'>
                 <div className='flex flex-col justify-center items-center'>
                     <Image src='/icons/django.png' width={100} height={100} alt="django logo"/>
                     <div>Django</div>
